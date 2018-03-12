83b191b7d6d00b0f06c3008b8f229f3bb58a8068
Kevin Huang <yellow.khaos@gmail.com>
2018-03-12 22:17:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+      enable :sessions
+      set :session_secret, "top_secret_phrase"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @session = session
+    @session["item"] = params[:item]
+
+    erb :cart
+  end
+
 end
